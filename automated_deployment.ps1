aaa8ed3694810b8695272185ffa604e5d81a0134
Samuel Eras <sa.eras@gmx.de>
2025-02-21 13:51:04 +0000

    fix rollback on error

--- automated_deployment.ps1
+++ automated_deployment.ps1
@@ -1,22 +1,30 @@
 $ErrorActionPreference = "Stop"
 
+Write-Output "Stopping port-forward..."
+Get-Job | Where-Object { $_.Command -match 'kubectl port-forward' } | Remove-Job -Force
+
 Write-Output "Rolling out changes..."
 kubectl rollout restart deployment kubernetes-assignment
 
 Write-Host "Waiting for deployment rollout..."
-$rolloutStatus = kubectl rollout status deployment/kubernetes-assignment --timeout=60s
 
-# Check if rollout was successful
-if ($rolloutStatus -match "successfully rolled out") {
-    Write-Host "Deployment successful!"
-} else {
-    Write-Host "Deployment failed! Rolling back..."
+try {
+
+    $rolloutStatus = kubectl rollout status deployment/kubernetes-assignment --timeout=60s 2>&1
+
+    $rolloutStatusString = $rolloutStatus -join "`n"
+
+    if ($rolloutStatusString -match "successfully rolled out") {
+        Write-Host "Deployment successful!"
+    } else {
+        Write-Host "Deployment failed! Rolling back..."
+        kubectl rollout undo deployment kubernetes-assignment
+    }
+}
+catch {
+    Write-Host "Deployment error or timeout occurred! Rolling back..."
     kubectl rollout undo deployment kubernetes-assignment
 }
 
-Get-Job | Where-Object { $_.Command -match 'kubectl port-forward' } | Remove-Job -Force
-
-Start-Sleep -Seconds 10
-
 Start-Job -ScriptBlock { kubectl port-forward svc/kubernetes-assignment 8080:8080 }
-Write-Output "Port forwarded and reachable."
+Write-Output "Port-forwarding started in the background."
